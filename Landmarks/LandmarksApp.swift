493ad14875c51e82493b47b806b1c5b941afc7d8
shota <68637411+vector-tech@users.noreply.github.com>
2021-05-09 14:57:40 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 松尾祥汰 on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
